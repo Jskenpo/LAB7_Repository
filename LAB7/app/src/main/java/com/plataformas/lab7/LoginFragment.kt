5a9e5c452c81d0e5adafd8aff6e508e53b055fb2
jsantis <san21153@uvg.edu.gt>
2022-08-28 22:56:34 -0600

    finishing details

--- LAB7/app/src/main/java/com/plataformas/lab7/LoginFragment.kt
+++ LAB7/app/src/main/java/com/plataformas/lab7/LoginFragment.kt
@@ -5,55 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var signin: Button
+    lateinit var signup: Button
+    lateinit var inputText: TextInputLayout
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        signin = view.findViewById(R.id.login)
+        signup = view.findViewById(R.id.signin)
+        inputText = view.findViewById(R.id.textinput)
+
+        listeners()
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun listeners() {
+        signin.setOnClickListener(){
+            var correo: String = inputText.editText!!.text.toString()
+            if(correo == "jcdurini@uvg.edu.gt"){
+                val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment(mail= correo)
+                view?.findNavController()?.navigate(action)
+            }else{
+                Toast.makeText(context, "El correo ingresado no es correcto", Toast.LENGTH_LONG).show()
             }
+
+        }
+
+        signup.setOnClickListener(){
+            val action = LoginFragmentDirections.actionLoginFragmentToSigninFragment()
+            requireView().findNavController().navigate(action)
+        }
+
     }
 }
